f74126e3c4dbdf1a8ed0bac4d617a1e494461832
Negar <babashahNegar@gmail.com>
2024-01-27 17:20:19 +0330

    Add interpreter

---
+++ interpreter.rkt
@@ -0,0 +1,14 @@
+(define (value-of exp env)
+    (case exp
+        (statement? (cases exp
+            ((assign var expr)(
+                ....
+            ))
+            ()
+            ...
+        )
+        (..?)
+    )
+    )
+)
+
