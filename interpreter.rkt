db71e2d33ab8e22715ed9b211312a92fbc13a894
kiana-msz <kianamousazadeh2018@gmail.com>
2024-01-29 22:20:10 +0330

    lazy func param

--- interpreter.rkt
+++ interpreter.rkt
@@ -118,7 +118,7 @@
             (eval-func-params 
                 (cases func_param param
                     (with_default (var expr)
-                        (eval_with_default var (value-of expr scope-index))))
+                        (eval_with_default var (a-thunkk expr scopes scope-index))))
                 (params-list rest-params scope-index)))))
 
 (define (extend-scope-with-params func-params in-params scope-index calling-scope-index)
@@ -141,7 +141,7 @@
                     (cases eval-func-param eval-param
                         (eval_with_default (var val)
                             (begin
-                                (extend-scope scope-index var (value-of expr calling-scope-index))
+                                (extend-scope scope-index var (a-thunkk expr scopes scope-index))
                                 (extend-scope-with-params rest-evals rest-exprs scope-index calling-scope-index)
                                 (none)
                             ))))))))
@@ -157,7 +157,9 @@
                     ;;; [applied-params (get-applied-params params in-params)]
                       [new-scope (add-scope (child-scope parent-scope))])
                         (begin
-                            (extend-scope-with-params params in-params new-scope scope-index)
+                            (if (thunkk? params)
+                                (extend-scope-with-params (value-of-thunkk params) in-params new-scope scope-index)
+                                (extend-scope-with-params params in-params new-scope scope-index))
                             (interpret-program-block statements new-scope)))))))
 
 (define (value-of exp scope-index)
