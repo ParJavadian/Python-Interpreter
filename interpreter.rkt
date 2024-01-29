33bfa078f4cd5cae857ce6f031d62023e1b0a1ee
kiana-msz <kianamousazadeh2018@gmail.com>
2024-01-30 01:09:51 +0330

    lazy func params + in-params

--- interpreter.rkt
+++ interpreter.rkt
@@ -158,9 +158,13 @@
             (new-proc (params statements parent-scope)
                 (let ([new-scope (add-scope (child-scope parent-scope))])
                         (begin
-                            (if (thunkk? in-params)
-                                (extend-scope-with-params params (value-of-thunkk in-params) new-scope scope-index)
-                                (extend-scope-with-params params in-params new-scope scope-index))
+                            (if (and (thunkk? in-params) (thunkk? params))
+                                (extend-scope-with-params (value-of-thunkk params) (value-of-thunkk in-params) new-scope scope-index)
+                                (if (thunkk? in-params)
+                                    (extend-scope-with-params params (value-of-thunkk in-params) new-scope scope-index)
+                                    (if (thunkk? params)
+                                        (extend-scope-with-params (value-of-thunkk params) in-params new-scope scope-index)
+                                        (extend-scope-with-params params in-params new-scope scope-index))))
                             (interpret-program-block statements new-scope))))))
 )
 
